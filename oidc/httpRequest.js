f51411ea9d98ed0ba8fed32c6f8d29deff7dc719
Dylan Marsh <dmarsh@overdrive.com>
2015-07-14 16:43:15 -0400

    initial commit

---
+++ oidc/httpRequest.js
@@ -0,0 +1,46 @@
+var promise = require('rsvp');
+var rp = require("request-promise");
+
+var exports = module.exports = {
+    /**
+     * @name _promiseFactory
+     * @type DefaultPromiseFactory
+     */
+
+    /**
+     * @param {XMLHttpRequest} xhr
+     * @param {object.<string, string>} headers
+     */
+    setHeaders: function (xhr, headers) {
+        var keys = Object.keys(headers);
+
+        for (var i = 0; i < keys.length; i++) {
+            var key = keys[i];
+            var value = headers[key];
+
+            xhr.setRequestHeader(key, value);
+        }
+    },
+
+    /**
+     * @param {string} url
+     * @param {{ headers: object.<string, string> }} [config]
+     * @returns {Promise}
+     */
+    getJSON : function (url, config) {
+        
+        // set headers in advanced scenario
+        var options = {
+            uri: url,
+            strictSSL: false,
+            json: true,
+            //proxy: 'http://localhost:8888'
+        };
+        
+        return rp(options).then(function(jsonObject){
+            return jsonObject;
+        }, function(error){
+            return error;
+        }).catch(console.error);;
+    }
+}
